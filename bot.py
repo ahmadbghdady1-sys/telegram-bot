2327eb54237a35a7c374571bf3cb83f140a6866f
ahmadbghdady1-sys <ahmadbghdady1@gmail.com>
2026-08-29 05:17:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def ask_groq(prompt):
         "Content-Type": "application/json"
     }
     payload = {
-        "model": "llama-3.3-70b-versatile",
+        "model": "gemma2-9b-it",
     
         "messages": [
             {"role": "system", "content": "أنت مساعد ذكي ومحترف، تجيب بدقة ووضوح باللغة العربية."},
